6bd09960f1fee5f127112a2deb2981d3aeb521a1
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-07-05 10:42:07 -0300

    refactor: updated sctucture project

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="django_sso_bridge",
-    version="0.1",
-    url="https://github.com/PedroHenriqueDevBR/django-sso-bridge",
-    license="Apache License 2.0",
-    author="PedroHenriqueDevBR",
-    author_email="pedro.henrique.particular@gmail.com",
+    packages=find_packages(include=["django_sso_bridge"]),
+    version="0.1.0",
     description="Bridge to connect django project with SSO System",
-    packages=find_packages(exclude=["tests"]),
+    author="PedroHenriqueDevBR",
+    license="Apache License 2.0",
+    install_requires=[],
+    url="https://github.com/PedroHenriqueDevBR/django-sso-bridge",
+    author_email="pedro.henrique.particular@gmail.com",
     long_description=open("README.md").read(),
     zip_safe=False,
 )
